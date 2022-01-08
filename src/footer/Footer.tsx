3a0518ea1cfa4ba1668ef12bfa42d5a53252fd01
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2022-01-08 23:39:29 +0300

    Added styles for Footer.tsx and Contact.tsx components

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -9,8 +9,37 @@ function Footer() {
 
 
     return (
-        <div className={style.footer}>
-            <div>Footer</div>
+        <div className={style.footerBlock}>
+            <div className={style.footerContainer}>
+                <div className={style.footerItems}>
+                    <div className={style.item}>
+                        <h5>Get in touch:</h5>
+                        <span>mona.mag@mail.ru</span>
+                        <div>+7(989)-671-2646</div>
+                    </div>
+                    <div className={style.item}>
+                        <h5>Social:</h5>
+                        <span>I'm in social media:</span>
+                        <div className={style.linkMedia}>
+                            <a href="#">linkedin</a>
+                            <a href="#">facebook</a>
+                            <a href="#">instagram</a>
+                        </div>
+                    </div>
+                    <div className={style.item}>
+                        <h5>Office:</h5>
+                        <span>367030 M.Gadgieva, 92, Makhachkala, RD RF.</span>
+                    </div>
+                    <div className={style.item}>
+                        <h5>Sigh up to newsletter:</h5>
+                        <form className={style.formFooter}>
+                            <input className={style.inputFooter} type="email"/>
+                            <button className={style.btnFooter}>SEND</button>
+                        </form>
+                    </div>
+                </div>
+               <div className={style.copyright}>@ 2021 | mona | All rights reserved</div>
+            </div>
         </div>
     )
 }
--- src/main/contact/Contact.tsx
+++ src/main/contact/Contact.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
-
+import style from './Contact.module.css'
 
 
 
 function Contact() {
 
-
     return (
-        <div>
-            <div>Contact</div>
+        <div className={style.contactBlock}>
+
+            <div className={style.contactContainer}>
+                <h2 className={style.contactTitle}>Contact</h2>
+                <form className={style.contactForm}>
+                    <input type="text" placeholder="Your name"/>
+                    <input type="email" placeholder="Your E-mail"/>
+                    <textarea placeholder="Message"/>
+                    <button type='submit' className={style.btnSendContact}>Send</button>
+                </form>
+            </div>
+
         </div>
+
     )
 }
 
